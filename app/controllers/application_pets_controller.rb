73f74769fcf259328df259ac5044550a8ee8e4b4
Laura Court <laura.mcourt@gmail.com>
2021-09-04 19:04:20 -0400

    Add pet to application

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationPetsController < ApplicationController
 
+  def create
+    pet = Pet.find(params[:pet_id])
+    app = Application.find(params[:id])
+    ApplicationPet.create!(pet: pet, application: app)
+    redirect_to "/applications/#{app.id}"
+  end
 end
