ccb782dfc887d7bb9b2c2742f8ce16b75b7f9120
Laura Court <laura.mcourt@gmail.com>
2021-09-03 23:04:20 -0400

    Remove user story

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -19,13 +19,6 @@ RSpec.describe "new application" do
     expect(page).to have_content(32819)
   end
 
-#   As a visitor
-# When I visit the new application page
-# And I fail to fill in any of the form fields
-# And I click submit
-# Then I am taken back to the new applications page
-# And I see a message that I must fill in those fields.
-
   it 'flashes an error' do
     visit "/applications/new"
     fill_in(:name, with: "Laura")
