52099621267e65dbb0306970ae3cb79b338a3ba0
Laura Court <laura.mcourt@gmail.com>
2021-09-07 18:38:04 -0400

    Adds story to spec

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -33,4 +33,14 @@ RSpec.describe 'admin application show page' do
       expect(page).to have_content("Rejected")
     end
   end
+
+#   As a visitor
+# When a pet has an "Approved" application on them
+# And when the pet has a "Pending" application on them
+# And I visit the admin application show page for the pending application
+# Then next to the pet I do not see a button to approve them
+# And instead I see a message that this pet has been approved for adoption
+# And I do see a button to reject them
+
+
 end
